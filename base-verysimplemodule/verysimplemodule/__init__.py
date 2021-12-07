6f4fc63eceb310e668a6a6397c5d3650455e2a68
andersm9 <m_anderson9@YAHOO.CO.UK>
2021-12-07 13:09:46 +0000

    Update __init__.py

--- base-verysimplemodule/verysimplemodule/__init__.py
+++ base-verysimplemodule/verysimplemodule/__init__.py
@@ -1,4 +1,4 @@
 from verysimplemodule.add import add
 from verysimplemodule.subtract import subtract
-from verysimplemodule.extras import divide
-from verysimplemodule.extras import multiply
+from verysimplemodule.extras.divide import divide
+from verysimplemodule.extras.divide import multiply
