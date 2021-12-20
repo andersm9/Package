e66012dcd82510f927e2c7cb77dd0e6b459801ee
andersm9 <m_anderson9@YAHOO.CO.UK>
2021-12-20 09:27:09 +0000

    Update add.py

--- base-verysimplemodule/verysimplemodule/add.py
+++ base-verysimplemodule/verysimplemodule/add.py
@@ -1,11 +1,14 @@
 class ComplexSums:
     
+    pi = 3.14
+    
     def __init__(self, a):
         print("Class instansiation")
         self.a = a
         
     def square(self):
         print(self.a**2)
+        print(pi)
        
 def add(a,b):
     print(a + b)
