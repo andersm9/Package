d56492c73e3781cafa7be58214e7f21f4d8e4fc0
andersm9 <m_anderson9@YAHOO.CO.UK>
2021-12-20 09:01:59 +0000

    Update add.py

--- base-verysimplemodule/verysimplemodule/add.py
+++ base-verysimplemodule/verysimplemodule/add.py
@@ -1,10 +1,11 @@
-class ComplexSums():
+class ComplexSums(a,b):
     
     def __init__(self):
         print("Class instansiation")
         
     def squareRoot():
-        print("squareRoot")
+        print("squareRoot of a = ")
+        print(a)
 
 def add(a,b):
     print(a + b)
